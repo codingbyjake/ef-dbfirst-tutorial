81877616f550c5a2b8302695971ab6d151cc0eeb
Jacob Witte <wittejg@gmail.com>
2023-02-23 08:56:38 -0500

    *added orderWithCustomers LINQ Quary Syntax code to Program.c *added CustomerController.cs to Project **added GetAllAsync, GetByIdAsync, InstertAsync, UpdateAsync, DeleteAsync methods to CustomerController class *added code in Program.cs to test

--- ef-dbfirst-tutorial/Program.cs
+++ ef-dbfirst-tutorial/Program.cs
@@ -1,22 +1,77 @@
 ﻿
+using ef_dbfirst_tutorial;
 using ef_dbfirst_tutorial.Models;
+using Microsoft.EntityFrameworkCore;
 
-var dbc = new SalesDbContext();
+var custCtrl = new CustomersController();
 
-var orderWithCustomers = from o in dbc.Orders
-                         join c in dbc.Customers
-                            on o.CustomerId equals c.Id
-                         orderby c.Name
-                         select new {
-                             Id = o.Id,
-                             Desc = o.Description,
-                             Customer = c.Name
-                         };
+//var bootcamp = await custCtrl.GetByIdAsync(39);
 
-foreach(var oc in orderWithCustomers) {
-    Console.WriteLine($"{oc.Id,2} | {oc.Desc,-30} | {oc.Customer,-30}");
-}
+//bootcamp.Sales = 5000;
 
+var success = await custCtrl.DeleteAsync(39);
+
+//var success = await custCtrl.UpdateAsync(bootcamp);
+
+//var cust = new Customer() {
+//    Id = 0,
+//    Name = "Bootcamp",
+//    City = "Mason",
+//    State = "OH",
+//    Sales = 0,
+//    Active = true
+//};
+
+//var success = await custCtrl.InsertAsync(cust);
+
+Console.WriteLine(success ? "OK" : "Failed");
+
+
+
+//var cust = await custCtrl.GetByIdAsync(1);
+
+//Console.WriteLine(cust.Name);
+
+//foreach(var cust in await custCtrl.GetAllAsync()) {
+//    Console.WriteLine(cust.Name);
+//}
+
+
+//var dbc = new SalesDbContext();
+
+////var customer = await GetById(5);
+
+//var customers = await GetAll();
+
+//foreach(var cust in customers) {
+//    Console.WriteLine(cust.Name);
+//}
+
+
+//async Task<Customer> GetById(int id) {
+//    return await dbc.Customers.FindAsync(id);
+//}
+
+//async Task<List<Customer>> GetAll() {
+//    return await dbc.Customers.ToListAsync();
+//}
+
+//linq query syntax with join
+//var orderWithCustomers = from o in dbc.Orders
+//                         join c in dbc.Customers
+//                            on o.CustomerId equals c.Id
+//                         orderby c.Name
+//                         select new {
+//                             Id = o.Id,
+//                             Desc = o.Description,
+//                             Customer = c.Name
+//                         };
+
+//foreach(var oc in orderWithCustomers) {
+//    Console.WriteLine($"{oc.Id,2} | {oc.Desc,-30} | {oc.Customer,-30}");
+//}
+
+//linq query syntax
 //var orders = from o in dbc.Orders
 //             select o;
 
@@ -24,6 +79,7 @@ foreach(var oc in orderWithCustomers) {
 //    Console.WriteLine(order.Description);
 //}
 
+//linq method syntax
 //var customers = dbc.Customers.Where(x => x.City == "Cincinnati")
 //                             .OrderByDescending(x => x.Sales)
 //                             .ToList();
