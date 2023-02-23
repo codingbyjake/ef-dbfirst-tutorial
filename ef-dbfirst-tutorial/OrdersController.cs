2a936e227c654fb658a3dce2cb5e3dd610e44c51
Jacob Witte <wittejg@gmail.com>
2023-02-23 12:36:36 -0500

    *refactored OrderControllers **added SaveDb and deleted duplicate code

---
+++ ef-dbfirst-tutorial/OrdersController.cs
@@ -0,0 +1,56 @@
+﻿using ef_dbfirst_tutorial.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_dbfirst_tutorial {
+    public class OrdersController {//begining of class
+
+        private readonly SalesDbContext _context;
+
+        public OrdersController() {
+            _context = new SalesDbContext();
+        }
+
+        public async Task<List<Order>> GetAllAsync() {
+            return await _context.Orders.ToListAsync();
+        }
+
+        public async Task<Order> GetByIdAsync(int id) {
+            return await _context.Orders.FindAsync(id);
+        }
+
+        public async Task<bool> InsertAsync(Order order) {
+            _context.Orders.Add(order);
+            return await SaveDb();
+        }
+
+        //UpdateAsync slightly diff than customer vers
+        public async Task<bool> UpdateAsync(Order order) {
+            var ord = await GetByIdAsync(order.Id);
+            if(ord is null) {
+                return false;
+            }
+            _context.Orders.Entry(order).State = EntityState.Modified;
+            return await SaveDb();
+        }
+
+        public async Task<bool> DeleteAsync(int id) {
+            var ord = await GetByIdAsync(id); 
+                if (ord is null) {
+                    return false;
+                }           
+            _context.Orders.Remove(ord);
+            return await SaveDb();
+        }     
+        
+        private async Task<bool> SaveDb() {
+            var changes = await _context.SaveChangesAsync();
+            return (changes == 1) ? true : false;
+        }
+     
+    } //end of the class
+}
--- ef-dbfirst-tutorial/Program.cs
+++ ef-dbfirst-tutorial/Program.cs
@@ -2,15 +2,51 @@
 using ef_dbfirst_tutorial;
 using ef_dbfirst_tutorial.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
 
-var custCtrl = new CustomersController();
 
+//***********testing DeleteAsync
+//var ordCtrl = new OrdersController();
+//bool success = await ordCtrl.DeleteAsync(27);
+//Console.WriteLine(success ? "It deleted" : "Failure!!!");
+
+//***********testing UpdateAsync
+//var ordCtrl = new OrdersController();
+
+//var order = await ordCtrl.GetByIdAsync(28);
+
+//order.Description += " by JW";
+
+//bool success = await ordCtrl.UpdateAsync(order);
+
+//Console.WriteLine(success ? "It updated" : "Failure!!!");
+
+
+//*********testing InsertAsync 
+//var ordCtrl = new OrdersController();
+
+//var order = new Order() {
+//    Id = 0,
+//    Date = new DateTime(2023, 2, 23),
+//    Description = "New New Kroger Order",
+//    CustomerId = 1
+//};
+//bool success = await ordCtrl.InsertAsync(order);
+
+//Console.WriteLine(success ? "It added" : "Failure!!!");
+//Console.WriteLine(order);
+
+//var custCtrl = new CustomersController();
+
+//Console.WriteLine(success ? "OK" : "Failed");
+
+
+//Delete a row by Id asynchronously
+//var success = await custCtrl.DeleteAsync(390);
+
+//
 //var bootcamp = await custCtrl.GetByIdAsync(39);
-
 //bootcamp.Sales = 5000;
-
-var success = await custCtrl.DeleteAsync(39);
-
 //var success = await custCtrl.UpdateAsync(bootcamp);
 
 //var cust = new Customer() {
@@ -24,7 +60,7 @@ var success = await custCtrl.DeleteAsync(39);
 
 //var success = await custCtrl.InsertAsync(cust);
 
-Console.WriteLine(success ? "OK" : "Failed");
+
 
 
 
@@ -86,4 +122,29 @@ Console.WriteLine(success ? "OK" : "Failed");
 
 //foreach(var customer in customers) {
 //    Console.WriteLine(customer.Name);
+//}
+
+//*************Asynchronous program flow example: correct ver on greg's github
+//var _context = new DbContext(); // <<<< not right
+//var startingNumber = 100;
+//var sum = await SumNumber1to10();
+
+//var total = sum + startingNumber;
+
+//Console.WriteLine("Done...");
+
+//async Task<int> SumNumber1to10() {
+//   // await _context.Customers.ToListAsync();
+//    int sum = 0;
+//    sum += 1;
+//    sum += 2;
+//    sum += 3;
+//    sum += 4;
+//    sum += 5;
+//    sum += 6;     
+//    sum += 7;
+//    sum += 8;
+//    sum += 9;
+//    sum += 10;
+//    return sum;
 //}
